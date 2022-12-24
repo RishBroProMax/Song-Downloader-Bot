55bd98d66c5bb1724b05619b978d2b70fb895338
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-12-24 19:34:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from os import getenv
 
 # Fill This Var
 class Config(object):
-	APP_ID = int()
-	API_HASH = ""
-	BOT_TOKEN = ""
+	APP_ID = int(14055090)
+	API_HASH = "a46f7b439d0afa45b7a69fc450f754e9"
+	BOT_TOKEN = "5939355561:AAHKlChKcMas5saDq27Z0yjrDkuZ5AXNMJE"
 	BOT_USERNAME = "TGYTMusicDownloader_Bot."
 	BOT_NAME = "Youtube Downloader Bot"
-	OWNER = int(5262156299)
-	LOG_GRP = int(-1001766852309)
+	OWNER = int(5171347305)
+	LOG_GRP = int(-1001551504918)
 	GROUP = "EmoBotDevolopers"
-	MONGODB_URI = ""
+	MONGODB_URI = "mongodb+srv://rishbro:rishbro@cluster0.eiqoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
